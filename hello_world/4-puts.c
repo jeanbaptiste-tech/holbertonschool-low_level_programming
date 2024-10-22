60bb12fc1e659b0c73793f3d63f46b9ab2241e43
jeanbaptiste-tech <jeanbaptisteatangana@gmail.com>
2024-10-22 13:56:54 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
-i#include <stdio.h>
+#include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+       	puts("\"Programming is like building a multilingual puzzle");
+       	return (0);
 }
