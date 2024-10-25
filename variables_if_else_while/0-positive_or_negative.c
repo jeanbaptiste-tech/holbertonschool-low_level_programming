730a9d28a9b9d39aeb56dfc7cb437aa6fb623c49
jeanbaptiste-tech <jeanbaptisteatangana@gmail.com>
2024-10-25 16:22:26 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,23 +4,22 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-	{
-		write(1, "est positif\n", 12);
-	}
-	else if (n == 0)
-	{
-		write(1, "vaut zéro\n", 11);
-	}
-	else
-	{
-		write(1, "est négatif\n", 12);
-	}
-	return (0);
+    if (n > 0)
+    {
+        write(1, "est positif\n", 12);
+    }
+    else if (n == 0)
+    {
+        write(1, "vaut zéro\n", 11);
+    }
+    else
+    {
+        write(1, "est négatif\n", 12);
+    }
+    return (0);
 }
-
