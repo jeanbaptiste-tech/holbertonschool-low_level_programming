ddc63356110e2cd3acfb7a39d571abe93e6ef976
jeanbaptiste-tech <jeanbaptisteatangana@gmail.com>
2024-10-25 22:17:56 +0200

    Update 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line
+ * Return: 0 on success
+ */
+int main(void)
+{
+    int num;
+
+    /* Loop from 0 to 9 */
+    for (num = 0; num < 10; num++)
+    {
+        putchar(num + '0'); /* Convert integer to character */
+    }
+    putchar('\n'); /* Print newline */
+
+    return (0);
+}
