fa0ebdfd6f38b838cccf6df51b811c82ff0e6ffc
jeanbaptiste-tech <jeanbaptisteatangana@gmail.com>
2024-10-25 21:46:55 +0200

    Update 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,22 +9,25 @@
  */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+	
+    printf("%d ", n); // Print the number
 
-	if (n > 0)
-	{
-		printf(1, "est positif\n", 12);
-	}
-	else if (n == 0)
-	{
-		printf(1, "vaut zéro\n", 11);
-	}
-	else
-	{
-		printf(1, "est négatif\n", 12);
-	}
-	return (0);
+    if (n > 0)
+    {
+        printf("is positive\n");
+    }
+    else if (n == 0)
+    {
+        printf("is zero\n");
+    }
+    else
+    {
+        printf("is negative\n");
+    }
+
+    return (0);
 }
