05e0ce9a8a5a74f43d7ca00dd9c3bc7dda929a22
jeanbaptiste-tech <jeanbaptisteatangana@gmail.com>
2024-10-29 23:15:27 +0000

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - Vérifie si un caractère est un chiffre (de 0 à 9).
+ * @c: Le caractère à vérifier.
+ *
+ * Return: 1 si le caractère est un chiffre, 0 sinon.
+ */
+int _isdigit(int c)
+{
+    if (c >= '0' && c <= '9')
+        return (1);
+    else
+        return (0);
+}
